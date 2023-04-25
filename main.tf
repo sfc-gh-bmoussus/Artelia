1a9ba981c7b250eb1a3a7c6cee46f3bef7f68f8b
baptiste_snowflake <baptiste.moussus@snowflake.com>
2023-04-25 18:49:49 +0200

    added main.tf file

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "Baptiste_Snowflake"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
+
+
